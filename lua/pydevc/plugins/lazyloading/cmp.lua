6b4c2feda1dd364b4684e456013d5691253d81ea
PyDevC <anuskcse@gmail.com>
2026-03-17 08:04:34 -0400

    minimal install

--- lua/pydevc/plugins/lazyloading/cmp.lua
+++ lua/pydevc/plugins/lazyloading/cmp.lua
@@ -9,7 +9,6 @@ return {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-buffer",
-      { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
     },
     config = function()
       require "pydevc.plugins.cmp"
