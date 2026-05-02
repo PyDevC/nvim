0e4297491a05ee7ab569badd58988268342a8095
PyDevC <anuskcse@gmail.com>
2026-05-02 01:19:54 -0400

    render markdown without highlighting Headings

--- lua/pydevc/plugins/lazyloading/render-markdown.lua
+++ lua/pydevc/plugins/lazyloading/render-markdown.lua
@@ -1,21 +1,28 @@
 return {
-    "MeanderingProgrammer/render-markdown.nvim",
-    opts = {
-        code = {
-            sign = false,
-            width = "block",
-            right_pad = 1,
-        },
-        heading = {
-            sign = false,
-            icons = {},
-        },
-        checkbox = {
-            enabled = false,
-        },
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {
+    heading = {
+      enabled = false,
     },
-    ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
-    config = function(_, opts)
-        require("render-markdown").setup(opts)
-    end,
+    paragraph = {
+      enabled = false,
+    },
+    code = {
+      sign = false,
+      width = "block",
+      right_pad = 1,
+      conceal_delimiters = false,
+      language_icon = true,
+    },
+    checkbox = {
+      enabled = true,
+    },
+    links = {
+      enabled = false
+    },
+  },
+  ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
+  config = function(_, opts)
+    require("render-markdown").setup(opts)
+  end,
 }
